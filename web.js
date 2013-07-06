1f7c1a280fdf0466c19d61b62da05c0aece1ffb8
Xiaoshu Qian <xiaoshu.qian@gmail.com>
2013-07-06 17:20:18 +0000

    added index.html

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var infile = 'index.html';
+  var str = fs.readFileSync(infile);
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
